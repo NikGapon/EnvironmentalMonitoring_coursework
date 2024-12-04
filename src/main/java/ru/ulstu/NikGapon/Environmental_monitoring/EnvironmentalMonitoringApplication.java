45e231c66382852113c6da8176e3a844160bfd5a
NikGapon <nik.gapon84@gmail.com>
2024-12-04 13:23:05 +0400

    init

---
+++ src/main/java/ru/ulstu/NikGapon/Environmental_monitoring/EnvironmentalMonitoringApplication.java
@@ -0,0 +1,13 @@
+package ru.ulstu.NikGapon.Environmental_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentalMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentalMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ulstu/NikGapon/Environmental_monitoring/EnvironmentalMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulstu.NikGapon.Environmental_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentalMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
